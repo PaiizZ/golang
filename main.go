a01603cc6cb8d8ab096aa67242d1e557e84cbf8e
paiizz <wanchanapon.t@ku.th>
2018-07-10 19:31:30 +0700

    reverseArray

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello")
+	reverseArray()
+}
+
+func reverseArray() {
+	num := []int{5, 2, 6, 3, 1, 4}
+
+	for i, j := 0, len(num)-1; i < j; i, j = i+1, j-1 {
+		num[i], num[j] = num[j], num[i]
+	}
+
+	fmt.Println(num)
+}

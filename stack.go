df1d95252a6beecf578c02863423d4f3a4d213a5
paiizz <wanchanapon.t@ku.th>
2018-07-10 23:42:17 +0700

    stack func

---
+++ stack.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type stack []int
+
+func main() {
+	var s stack
+	s = s.push(5)
+	s = s.push(4)
+	s = s.push(3)
+	s = s.push(2)
+	s = s.push(1)
+	s, _ = s.pop()
+	fmt.Println(s)
+	fmt.Println(len(s))
+}
+
+func (s stack) push(a int) stack {
+	return append(s, a)
+}
+
+func (s stack) pop() (stack, error) {
+	if len(s) == 0 {
+		return s, errors.New("Empty Stack")
+	}
+	s = s[:len(s)-1]
+	return s, nil
+}

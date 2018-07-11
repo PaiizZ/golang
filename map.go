89bb59beec293a47cd882d097ff5a2cdda7dbc18
paiizz <wanchanapon.t@ku.th>
2018-07-11 19:49:13 +0700

    map go

---
+++ map.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	arr := strings.Fields(s)
+	strs := make(map[string]int)
+	for i := 0; i < len(strings.Fields(s)); i++ {
+		if _, ok := strs[arr[i]]; ok {
+			strs[arr[i]]++
+		} else {
+			strs[arr[i]] = 1
+		}
+	}
+	return strs
+}
+
+func WordCountTon(s string) map[string]int {
+	strs := make(map[string]int)
+	for _, str := range strings.Fields(s) {
+		strs[str]++
+	}
+	return strs
+}
+
+func main() {
+	fmt.Println(WordCountTon("I LOVE U"))
+	fmt.Println(WordCount("I LOVE U"))
+}
